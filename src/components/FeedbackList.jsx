e076907bd790c62499df0b7cdd2cbe2cc8d2c79e
Annie Huang <annie.hn@gmail.com>
2022-01-16 10:32:46 +1100

    Update FeedbackList.jsx. If there is not feedback passing in. show special msg. Otherwise construct a <FeedbackItem> list.

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,7 +1,18 @@
 import React from 'react';
+import FeedbackItem from './FeedbackItem';
 
-const FeedbackList = () => {
-  return <div>List</div>;
+const FeedbackList = ({ feedback }) => {
+  if (!feedback || feedback.length === 0) {
+    return <p>No Feedback Yet</p>;
+  }
+
+  return (
+    <div className='feedback-list'>
+      {feedback.map((item) => (
+        <FeedbackItem key={item.id} item={item} />
+      ))}
+    </div>
+  );
 };
 
 export default FeedbackList;
