990bdbcf8ec648e486ee3299aaf4f8967137cebb
Annie Huang <annie.hn@gmail.com>
2022-01-16 10:43:20 +1100

    Create a <Card> component just for styling, and it will display the children it passed in.

--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -1,11 +1,12 @@
-import React, { useState } from 'react';
+import React from 'react';
+import Card from './shared/Card';
 
 const FeedbackItem = ({ item }) => {
   return (
-    <div className='card'>
+    <Card>
       <div className='num-display'>{item.rating}</div>
       <div className='text-display'>{item.text}</div>
-    </div>
+    </Card>
   );
 };
 
