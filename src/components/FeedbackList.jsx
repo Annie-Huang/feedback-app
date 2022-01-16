05344c0eb78f339126e0a146805a1fedbc57f854
Annie Huang <annie.hn@gmail.com>
2022-01-16 16:48:10 +1100

    Add animation for add and remove card from framer-motion

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,3 +1,4 @@
+import { motion, AnimatePresence } from 'framer-motion';
 import React from 'react';
 import FeedbackItem from './FeedbackItem';
 import PropTypes from 'prop-types';
@@ -7,11 +8,30 @@ const FeedbackList = ({ feedback, handleDelete }) => {
     return <p>No Feedback Yet</p>;
   }
 
+  // // Version without animation.
+  // return (
+  //   <div className='feedback-list'>
+  //     {feedback.map((item) => (
+  //       <FeedbackItem key={item.id} item={item} handleDelete={handleDelete} />
+  //     ))}
+  //   </div>
+  // );
+
+  // Animation is available for both add and remove a card.
   return (
     <div className='feedback-list'>
-      {feedback.map((item) => (
-        <FeedbackItem key={item.id} item={item} handleDelete={handleDelete} />
-      ))}
+      <AnimatePresence>
+        {feedback.map((item) => (
+          <motion.div
+            key={item.id}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+          >
+            <FeedbackItem item={item} handleDelete={handleDelete} />
+          </motion.div>
+        ))}
+      </AnimatePresence>
     </div>
   );
 };
