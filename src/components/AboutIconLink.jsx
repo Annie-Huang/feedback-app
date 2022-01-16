6961dc7d92ba565b1eb101a227fedb05ceb8b4a1
Annie Huang <annie.hn@gmail.com>
2022-01-16 21:15:19 +1100

    Different variants for the <Link>

--- src/components/AboutIconLink.jsx
+++ src/components/AboutIconLink.jsx
@@ -4,7 +4,8 @@ import { Link } from 'react-router-dom';
 const AboutIconLink = () => {
   return (
     <div className='about-link'>
-      <Link to='about'>
+      {/*<Link to='about'>*/}
+      <Link to={{ pathname: '/about', search: '?sort=name', hash: '#hello' }}>
         <FaQuestion size={30} />
       </Link>
     </div>
