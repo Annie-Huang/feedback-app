16bc0baa281828bb6d806cbf1439420f6f3413ba
Annie Huang <annie.hn@gmail.com>
2022-01-16 12:09:33 +1100

    Add the icon of close into FeedbackItem.jsx and detect onClick.

--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
 import Card from './shared/Card';
 import PropTypes from 'prop-types';
+import { FaTimes } from 'react-icons/fa';
 
 const FeedbackItem = ({ item }) => {
+  const handleClick = (id) => {
+    console.log(id);
+  };
+
   return (
     <Card>
       <div className='num-display'>{item.rating}</div>
+      <button onClick={() => handleClick(item.id)} className='close'>
+        <FaTimes color='purple' />
+      </button>
       <div className='text-display'>{item.text}</div>
     </Card>
   );
